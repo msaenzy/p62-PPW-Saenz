601eca514e30c6343c289f2436673fcaf57e25be
Maria Fernanda Saenz <msaenzy@est.ups.edu.ec>
2023-06-22 20:12:09 -0500

    ult

--- Javascript/dom/taller_06/script.js
+++ Javascript/dom/taller_06/script.js
@@ -1,12 +1,12 @@
 function show() {
 			
     document.getElementById('image')
-        ;
+        .style.display = "block";
     document.getElementById('btnID')
         .style.display = "none";
 }
 
 function eliminar_elemento() {
-    var container = document.getElementById('container')
-    container.removeChild( container.lastElementChild )
+    var container = document.getElementById('image')
+    container.removeChild( image.lastElementChild )
 }
